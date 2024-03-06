6d37cadeb3cbc2dc783a90d0a19e31c18cd17685
Akshat Mishra <akshatmishra317@gmail.com>
2024-03-07 01:53:51 +0530

    provides backend to wherever needed

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import deleteUser from "./controllers/usersDeleter.js";
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-app.use(cors({origin: "http://localhost:3000"}));
+app.use(cors({origin: "*"}));
 
 mongoose.connect(process.env.MONGO_DB_URI)
   .then(() => console.log('Connected to MongoDB'))
